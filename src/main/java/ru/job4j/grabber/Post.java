095423bac01250492a738bd60c420e2929c4d01e
Vitas2048 <vithag97@mail.ru>
2022-10-31 14:49:19 +0700

    2.2. Модель данных - Post. [#285211 #356485]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -14,9 +14,59 @@ public class Post {
 
     private LocalDateTime created;
 
-    @Override
-    public String toString() {
-        return String.format("id = %s, title = %s, link = %s, description = %s, created = %s", id, title, link, description, created);
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     @Override
@@ -28,11 +78,17 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id;
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, link);
     }
+
+    @Override
+    public String toString() {
+        return String.format("id = %s, title = %s, link = %s, description = %s, created = %s", id, title, link, description, created);
+    }
+
 }
